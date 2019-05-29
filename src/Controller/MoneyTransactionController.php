6ee49f85698f9eb979bc788a9766096aca700578
Guido Pili <guido.pili@docebo.com>
2019-05-29 14:53:11 +0200

    Remove unused use

--- src/Controller/MoneyTransactionController.php
+++ src/Controller/MoneyTransactionController.php
@@ -14,7 +14,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Safebeat\Annotation\RequestBodyValidator;
 use Safebeat\Validator\{EmptyValidator, NumericValidator};
